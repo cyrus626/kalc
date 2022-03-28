435871ee6036214b38b82bcc6f83178291cd8262
cyrus626 <momohcyrus@gmail.com>
2022-03-28 17:51:48 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome to kalc
+            //this applicaton works as a console calculator
+            Intro();
+            Console.ReadKey();
+        }
+        
+        private static void Intro()
+        {
+            // Introduces the program
+            string message = "Hello, Welcome to Kalc \n " +
+                "\n Please select an operation:" +
+                "\n 1. Addition" +
+                "\n 2. Subtraction";
+            Console.Write(message);
+        }
+    }
+}
